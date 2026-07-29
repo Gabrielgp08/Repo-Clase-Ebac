539ffc48f261e2902d23569d5ac7e7f2487e7469
Gabrielgp08 <161524920+Gabrielgp08@users.noreply.github.com>
2026-07-29 14:22:24 -0500

    Srcipt Hola mundo
    Se utilizo
    Print
    Debug.log
    LogWarning
    LogError

---
+++ InitialProjectEbac/Assets/Scripts/HolaMundo.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HolaMundo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Algo paso");
+        Debug.Log("Algo paso");
+        Debug.LogWarning("Algo Salio Muy Mal");
+        Debug.LogError("Algo Terrible paso");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
